b0161ef145ffb537eb34e24353dc633e2c6b5f01
sukesan000 <oyama.k.1997@gmail.com>
2023-08-29 21:25:17 +0900

    諸々修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sukesan000.github.io",
   base: "/sukesan-blog",
+  integrations: [mdx()]
 });
